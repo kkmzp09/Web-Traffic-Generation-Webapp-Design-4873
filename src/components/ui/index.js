6d9d9cde5412bf2e140850b7a4e7dec40318a6fd
kkmzp09 <kkmzp09@gmail.com>
2025-10-20 18:59:33 -0700

    Revert "Add industry-standard UI component library with modern UX patterns"
    This reverts commit e5a47269f950d1ac31940186aefe67dbb8824284.

--- src/components/ui/index.js
+++
@@ -1,9 +0,0 @@
-// UI Component Library - Industry Standard Components
-// Export all reusable UI components
-
-export { default as Button } from './Button';
-export { default as Card } from './Card';
-export { default as Input } from './Input';
-export { default as EmptyState } from './EmptyState';
-export { default as LoadingSpinner, Skeleton, CardSkeleton, TableSkeleton } from './LoadingSpinner';
-export { ToastProvider, useToast } from './Toast';
